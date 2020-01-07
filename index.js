5e44ab9e40563dd7c1745caef5c6f6b9be80fd41
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2020-01-07 11:57:09 -0500

    avanzando

--- index.js
+++ index.js
@@ -6,26 +6,34 @@ const SSLStatus = async (hostname) => await sslChecker(hostname, {method: "GET",
 
 const stopServer = async (url) =>{
     const Status = await SSLStatus(url)
-    console.log("1")
     if(Status.daysRemaining >= 30) return Status
-    console.log("2")
-    const {stdout, stderr} = await exec("sudo /opt/bitnami/ctlscript.sh stop")
-    
-    console.log(stdout)
 
+    const {stdout, stderr} = await exec("sudo /opt/bitnami/ctlscript.sh stop")
+    if(stderr) return console.log(stderr)
+
+    console.log(stdout)
+    console.log(stderr)
     return true;
+}
+
+const startServer = async()=>{
+    const {stdout, stderr} = await exec("sudo /opt/bitnami/ctlscript.sh start")
+    return true
 }
 
 const renewCertificate = async (url)=>{
     const StopServer = await stopServer(url)
-    console.log("3")
     if(StopServer){
-        console.log("Listo para actualizar esta xhaxhu")
-        return true;
+        console.log("Listo para actualizar")
+        const {stdout,  stderr} = await exec(`sudo /opt/bitnami/letsencrypt/lego -tls --email="soporte@bluetideconsulting.com" --domains="${url}" --path="/opt/bitnami/letsencrypt"`)
+        const startServer = await startServer();
+    }
+    if(startServer){
+        return "Renewed"
     }
 }
 
-renewCertificate("nitocortizo.com")
+renewCertificate("bluetideconsulting.com")
         .then((result) => {
             console.log("4")
             console.info(result)
