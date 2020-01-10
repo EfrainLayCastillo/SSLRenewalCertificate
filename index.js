8669d1c18a8a09530c6fc6dfb9f52836b84e4c67
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2020-01-10 14:44:25 -0500

    cambios leves de status y funciones

--- index.js
+++ index.js
@@ -3,39 +3,57 @@ const exec =  util.promisify(require("child_process").exec);
 const sslChecker = require('ssl-checker');
 
 const SSLStatus = async (hostname) => await sslChecker(hostname, {method: "GET", port: "443"});
+const legoVersion = async ()=>{
+    const {stdout, stderr} = await exec("lego --version")
+    let number
+    if(stderr == ''){
+        number = stdout.split(" ")[2]
+        console.log(stdout);
+        number = number.charAt(0);
+        if( Number(number) < 2 ){
+            console.log("Se necesita actualizar el Lego");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
 
 const stopServer = async (url) =>{
-    const Status = await SSLStatus(url)
-    if(Status.daysRemaining >= 30) return Status
-
-    const {stdout, stderr} = await exec("sudo /opt/bitnami/ctlscript.sh stop")
-    if(stderr) return console.log(stderr)
-
-    console.log(stdout)
-    console.log(stderr)
-    return true;
+    return await exec("sudo /opt/bitnami/ctlscript.sh stop")
 }
 
 const startServer = async()=>{
-    const {stdout, stderr} = await exec("sudo /opt/bitnami/ctlscript.sh start")
-    return stdout;
+    return await exec("sudo /opt/bitnami/ctlscript.sh start")
+}
+
+const statusServer = async ()=>{
+    return await exec("sudo /opt/bitnami/ctlscript.sh status")
 }
 
 const renewCertificate = async (url)=>{
-    const StopServer = await stopServer(url)
-
-        console.log("Listo para actualizar")
+    let Status = await SSLStatus(url)
+    const legoV = await legoVersion()
+    if(Status.daysRemaining < 30 && legoV != true){
+        const responseStopServer = await stopServer(url)
+        console.log(responseStopServer.stdout);
         const {stdout,  stderr} = await exec(`sudo lego --tls --email="soporte@bluetideconsulting.com" --domains="${url}" --path="/etc/lego" renew --days 90`)
+        if(stderr != '') return stderr.toString()
         console.log(stdout.toString());
-        console.log(stderr.toString());
         console.log("Iniciando Servidor")
-        const Start = await startServer();
-        console.log(Start.toString());
-        console.log("Servidor Corriendo")
+        const  responseStartServer = await startServer();
+        console.log(responseStartServer.stdout);
+        const responseStatusServer = await statusServer();
+        console.log(responseStatusServer.stdout);
+    }else{
+        return console.log(`${Status} ${legoV}`)
+    }
 }
 
 renewCertificate("lacayolaw.com")
         .then((result) => {
+            console.log("Servidor Corriendo")
             console.info(result)
         }).catch(e =>{
             console.info(e)
